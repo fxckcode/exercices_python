099de153e6f7a9de82934743d02a58197b13fef0
fxckcode <diegoale8582@hotmail.com>
2023-06-12 17:35:29 -0500

    finish 1 rate

---
+++ ejericios_de_tipos_de_datos/ejercicios.py
@@ -0,0 +1,61 @@
+# URL https://aprendeconalf.es/docencia/python/ejercicios/tipos-datos/
+# Ejercicio 1
+# print("¡Hola mundo!") - Finalizado
+
+# Ejercicio 2
+# variable = "¡Hola mundo!"
+# print(variable) - Finalizado
+
+# Ejercicio 3
+# nombre = str(input("Cual es tu nombre: "))
+# print(f"¡Hola {nombre}!") - Finalizado
+
+# Ejercicio 4
+# print(((3+2) / (2*5))**2) - Dinalizado
+
+# Ejericio 5
+# numeroHoras = int(input("Cuantas horas trabajaste?: "))
+# valorHora = int(input("Cuanto vale un hora de tu trabajo?: "))
+# print(f"El valor a pagar es {numeroHoras * valorHora}")  - Finalizado
+
+# Ejercicio 6
+# numero = int(input("Dame un numero: "))
+# operacion = (numero * (numero + 1)) / 2
+# print(operacion) - Finalizado
+
+# Ejercicio 7
+# peso = float(input("Cual es tu peso en Kg: "))
+# estatura  = float(input("Cual es tu estatura en metros: "))
+# calculo = float(peso / estatura**2)
+# print(round(calculo, 2)) - Finalizado
+
+# Ejercicio 8
+# n = int(input("Primer numero entero: "))
+# m = int(input("Segundo numero entero: "))
+# print(f"{n} entro {m} da un conciente {str(n // m)} y un resto {str(n % m)}") - Finalizado
+
+# Ejercicio 9
+# cantidad = int(input("Cantidad a invertir: "))
+# interesA = float(input("Tasa de interes anual?: "))
+# numeroYears = int(input("Numero de años"))
+# print(f"Ganacia {round(cantidad * (interesA / 100 + 1)**numeroYears , 2)}") - Finalizado
+
+# Ejercicio 10
+# payaso = 0.112
+# muñeca = 0.075
+# nPayaso = int(input("Numero de payasos: "))
+# nMuñecas = int(input("Numero de muñecas: "))
+# print(f"Peso total {round((nPayaso * payaso) + (nMuñecas * muñeca), 2)} Kg") - Finalizado
+
+# Ejercicio 11
+# cantidad = int(input("Cantidad de ahorros: "))
+# interes = 0.04
+# calculosInteres = cantidad * interes
+# print(f"Calculo: \n Primer año {round(cantidad + calculosInteres, 2)} \n Segundo año {round((cantidad + calculosInteres) + calculosInteres, 2)} \n Tercer año {round((cantidad + calculosInteres) + calculosInteres * 2, 2)}") - Finalizado
+
+# Ejercicio 12
+# barraPanFresco = 3.49
+# descuento = 0.60
+# barras = int(input("Barras de pan: "))
+# print(f"Precio habitual: {barraPanFresco} Euros, descuento 60%, total {round((barraPanFresco - (barraPanFresco * descuento)) * barras, 2)}") - Finalizado
+
